1007c41db138f939f2a6b3bf2e59bd1450c3a43e
@devCluna <jobs.cluna@gmail.com>
2022-03-13 12:37:58 -0500

    ✨ (newsSelector, comp) add picker news selector feature @devCluna

--- src/components/NewsSelector/NewsSelector.jsx
+++ src/components/NewsSelector/NewsSelector.jsx
@@ -4,10 +4,14 @@ import arrow from '../../assets/svg/up-arrow.svg'
 import angularIcon from '../../assets/svg/image-138@3x.png'
 import reactIcon from '../../assets/svg/image-140@3x.png'
 import vueIcon from '../../assets/svg/image-141@3x.png'
+import { useDispatch } from 'react-redux'
+import { pickNews } from '../../redux/newsSelector/newsSelectorActions'
 
 const NewsSelector = () => {
     const [toggleSelector, setToggleSelector] = useState(false)
+    const [optionSelector, setOptionSelector] = useState()
     const selectorRef = useRef()
+    const pickNewsDispatch = useDispatch()
 
     //click outside to close toggle
     useEffect(()=>{
@@ -26,25 +30,31 @@ const NewsSelector = () => {
         <div ref={selectorRef} className="newsSelector-holder" onClick={()=>{
             setToggleSelector(!toggleSelector)
         }}>
-            Select your news
+            {!optionSelector ? "Select your news" : optionSelector}
             <img src={arrow}/>
         </div>
 
         <div className={toggleSelector ? 'newsSelector-options': 'unselected-options'}>
             <div className='newsSelector-option' onClick={()=>{
                 setToggleSelector(!toggleSelector)
+                setOptionSelector('Angular')
+                pickNewsDispatch(pickNews('angular'))
             }}>
                 <img className='newsSelector-option-icon' src={angularIcon}/>
                 Angular
             </div>
             <div className='newsSelector-option' onClick={()=>{
                 setToggleSelector(!toggleSelector)
+                setOptionSelector('React')
+                pickNewsDispatch(pickNews('react'))
             }}>
                 <img className='newsSelector-option-icon' src={reactIcon}/>
                 React
             </div>
             <div className='newsSelector-option' onClick={()=>{
                 setToggleSelector(!toggleSelector)
+                setOptionSelector('Vue')
+                pickNewsDispatch(pickNews('vue'))
             }}>
                 <img className='newsSelector-option-icon' src={vueIcon}/>
                 Vue
