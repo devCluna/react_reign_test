0ed37b82216ad5b3ebbfae330904d9afc4c49bce
@devCluna <jobs.cluna@gmail.com>
2022-03-13 12:26:34 -0500

    💄 add dinamic author name & post title

--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+
+import React, { useEffect } from "react";
 import "./NewsItem.css";
 import favIconDisable from '../../assets/svg/iconmonstr-favorite-2.svg'
 import favIcon from '../../assets/svg/iconmonstr-favorite-3.svg'
 import watchIcon from '../../assets/svg/iconmonstr-time-2.svg'
 
-const NewsItem = ({picked}) => {
+const NewsItem = ({picked, data}) => {
+
   return (
     <div className="newsItem">
       <div className="newsItem-body">
         <div className="newsItem-head">
           <img className="newsItem-watchIcon" src={watchIcon}/>
-          <span className="newsItem-author-time">2 hours ago by author</span>
+          <span className="newsItem-author-time">2 hours ago by {data.author} </span>
         </div>
 
-        <div className="newsItem-description">From chaos to free will</div>
+        <div className="newsItem-description">{data.story_title ? data.story_title : data.title}</div>
       </div>
 
       <div className="newsItem-button-holder">
