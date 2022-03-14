579a1562acbd7336139f9018e55092feb3a54547
@devCluna <jobs.cluna@gmail.com>
2022-03-13 23:06:29 -0500

    ♻️ change toggle display news with global store @devCluna

--- src/components/NewsContainer/NewsContainer.jsx
+++ src/components/NewsContainer/NewsContainer.jsx
@@ -10,8 +10,9 @@ import { fetchNews } from '../../redux/newsSelector/newsSelectorActions'
 const NewsContainer = () => {
   const dispatch= useDispatch()
   const state = useSelector(state => state)
+  const navOption = useSelector(state => state.navOptions.currentOption)
   const newsData = useSelector(state => state.newsSelector.newsData)
-  const [pageNumber, setPageNumber] = useState(0)
+  const likedNews = useSelector(state => state.favNews.likedNews)
 
   const pageCount = Math.ceil(newsData.nbPages / newsData.hitsPerPage)
 
@@ -21,49 +22,43 @@ const NewsContainer = () => {
   }
 
   return (
-    <div style={{display: "flex", flexDirection: "column"}}>
+    <>
+      {navOption 
+      ?
+      <div style={{display: "flex", flexDirection: "column"}}>
       <div className="newsContainer">
       
-      {newsData.hits?.map(item => (
-        <NewsItem key={item.objectID} picked={true} data={item}/>
-      ))}
+      {newsData.hits?.map(item => {
+        
+        return (<NewsItem key={item.objectID} picked={likedNews.some(news => news.objectID === item.objectID ? true: false)} data={item}/>)
+      })}
       </div>
+   
+      <ReactPaginate
+        previousLabel={"<"}
+        nextLabel={">"}
+        pageCount={pageCount}
+        containerClassName ={"newsPage-counter"}
+        onPageChange={changePages}
+        pageClassName={"pages-item"}
+        breakClassName={"pages-item"}
+        previousLinkClassName={"newsPage-arrow-counter"}
+        nextLinkClassName={"newsPage-arrow-counter"}
+        activeClassName={"page-item-selected"}
+      />
 
-      <div className='newsPage-counter'>
-        
-        {/* <div className='newsPage-arrow-counter'>
-          <img src={arrow} style={{'transform': "rotate(270deg)"}}/>
-        </div>
-
-        <div className='pages-container' style={{display:"flex"}}>
-          <div className='pages-item'>{currentPage+1}</div>
-          <div className='pages-item page-item-selected'>{currentPage+2}</div>
-          <div className='pages-item'>{currentPage+3}</div>
-          <div className='pages-item'>{currentPage+4}</div>
-          <div className='pages-item'>{currentPage+5}</div>
-          <div className='pages-item'>{currentPage+6}</div>
-
-        </div>
-        
-
-        <div className='newsPage-arrow-counter'>
-          <img className='arrow-slider' src={arrow} style={{'transform': "rotate(90deg)"}}/>
-        </div> */}
-
-        <ReactPaginate
-          previousLabel={"<"}
-          nextLabel={">"}
-          pageCount={pageCount}
-          containerClassName ={"newsPage-counter"}
-          onPageChange={changePages}
-          pageClassName={"pages-item"}
-          breakClassName={"pages-item"}
-          previousLinkClassName={"newsPage-arrow-counter"}
-          nextLinkClassName={"newsPage-arrow-counter"}
-          activeClassName={"page-item-selected"}
-        />
+    </div>
+    : 
+    <div style={{display: "flex", flexDirection: "column"}}>
+      <div className="newsContainer">
+      {likedNews.map(item => {
+        return (<NewsItem key={item.objectID} picked={likedNews.some(news => news.objectID === item.objectID ? true: false)} data={item}/>)
+      })}
       </div>
     </div>
+    }
+    
+    </>
   )
 }
 
