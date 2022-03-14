2c7128b0aac91baf104f45544549e63b1e1f1c6a
@devCluna <jobs.cluna@gmail.com>
2022-03-13 20:20:59 -0500

    👔 add pagination reducer to main store @devCluna

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,11 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import newsSelectorReducer from "./newsSelector/newsSelectorReducer";
+import newsPaginationReducer from "./newsPagination/newsPaginationReducer";
 
 const  rootReducer = combineReducers({
-    newsSelector: newsSelectorReducer
+    newsSelector: newsSelectorReducer,
+    newsPagination: newsPaginationReducer
 })
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
