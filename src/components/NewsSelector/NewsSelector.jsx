d7bd4fa29131ffceeafc627cffea4eb3cdb78e91
@devCluna <jobs.cluna@gmail.com>
2022-03-13 20:26:55 -0500

    👔 add dynamic news selectio options @devCluna

--- src/components/NewsSelector/NewsSelector.jsx
+++ src/components/NewsSelector/NewsSelector.jsx
@@ -4,14 +4,36 @@ import arrow from '../../assets/svg/up-arrow.svg'
 import angularIcon from '../../assets/svg/image-138@3x.png'
 import reactIcon from '../../assets/svg/image-140@3x.png'
 import vueIcon from '../../assets/svg/image-141@3x.png'
-import { useDispatch } from 'react-redux'
-import { pickNews } from '../../redux/newsSelector/newsSelectorActions'
+import { useDispatch, useSelector } from 'react-redux'
+import {  pickNews, fetchNews} from '../../redux/newsSelector/newsSelectorActions'
 
 const NewsSelector = () => {
     const [toggleSelector, setToggleSelector] = useState(false)
     const [optionSelector, setOptionSelector] = useState()
     const selectorRef = useRef()
     const pickNewsDispatch = useDispatch()
+    const state = useSelector(state => state)
+
+    const arrayNewsSelector =[
+        {   
+            id:1,
+            displayName: "Angular",
+            name: "angular",
+            imgSrc:angularIcon
+        },
+        {
+            id:2,
+            displayName: "React",
+            name: "react",
+            imgSrc:reactIcon
+        },
+        {
+            id:3,
+            displayName: "Vue",
+            name: "vue",
+            imgSrc:vueIcon
+        },
+    ]
 
     //click outside to close toggle
     useEffect(()=>{
@@ -35,30 +57,18 @@ const NewsSelector = () => {
         </div>
 
         <div className={toggleSelector ? 'newsSelector-options': 'unselected-options'}>
-            <div className='newsSelector-option' onClick={()=>{
-                setToggleSelector(!toggleSelector)
-                setOptionSelector('Angular')
-                pickNewsDispatch(pickNews('angular'))
-            }}>
-                <img className='newsSelector-option-icon' src={angularIcon}/>
-                Angular
-            </div>
-            <div className='newsSelector-option' onClick={()=>{
-                setToggleSelector(!toggleSelector)
-                setOptionSelector('React')
-                pickNewsDispatch(pickNews('react'))
-            }}>
-                <img className='newsSelector-option-icon' src={reactIcon}/>
-                React
-            </div>
-            <div className='newsSelector-option' onClick={()=>{
-                setToggleSelector(!toggleSelector)
-                setOptionSelector('Vue')
-                pickNewsDispatch(pickNews('vue'))
-            }}>
-                <img className='newsSelector-option-icon' src={vueIcon}/>
-                Vue
-            </div>
+            {arrayNewsSelector.map(newsType => (
+                <div key={newsType.id} className='newsSelector-option' onClick={()=>{
+                    setToggleSelector(!toggleSelector)
+                    setOptionSelector(newsType.displayName)
+                    pickNewsDispatch(pickNews(newsType.name))
+                    pickNewsDispatch(fetchNews(state.newsSelector.news, state.newsPagination.currentPage))
+                }}>
+                    <img className='newsSelector-option-icon' src={newsType.imgSrc}/>
+                    {newsType.displayName}
+                </div>
+            ))
+            }
         </div>
     </div>
   )
